b8faec23e301c29269335f7ec0d665e26222df66
ZEESHAN0603 <06zeeshan03@gmail.com>
2026-06-07 21:20:46 +0530

    Server

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,11 +1,4 @@
-
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static String get baseUrl {
-    if (kDebugMode) {
-      return 'http://localhost:8000';
-    }
-    return 'https://synora-backend-rhi7.onrender.com';
-  }
+  static const String baseUrl =
+      'https://synora-backend-rhi7.onrender.com';
 }
